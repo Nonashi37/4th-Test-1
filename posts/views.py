a1b68235d596a7f5ec1ab353499ab44181c7ed52
Nonashi37 <argeno37@gmail.com>
2026-04-23 21:59:22 +0600

    now finally the Tags works the problmes was Tags.objects.filter(pk=tag_slug) returns a QuerySet, even if it contains only one tag. I wrote more detailed in posts/views.py

--- posts/views.py
+++ posts/views.py
@@ -2,6 +2,7 @@ from django.contrib.auth.decorators import login_required
 from django.contrib.auth.models import AnonymousUser
 from django.http import HttpRequest, HttpResponse
 from django.shortcuts import get_object_or_404, redirect, render
+from posts.models import Post, Tags
 
 from posts.forms import CommentForm, PostForm
 from posts.models import Post, Tags # add tags import
@@ -20,18 +21,18 @@ def about(request):
 
 
 def get_posts(request):
-    tag_slug = request.GET.get("tag") # ?tag=3 from URL
-    tags = Tags.objects.all()
+    tag_id = request.GET.get("tag") # Tags.objects.filter(pk=tag_slug) returns a QuerySet, even if it contains only one tag.
+    tags = Tags.objects.all()       # But posts.filter(tags=...) expects one tag object, not a QuerySet.
                                
     posts = Post.objects.filter(is_published=True).select_related("user").order_by("-created_at")
 
     selected_tag = None
-    if tag_slug:
-        selected_tag = Tags.objects.filter(pk=tag_slug).filter()
-        if selected_tag:
-            posts = posts.filter(tags=selected_tag)
-
-    return render(request, "posts/posts_view.html", context={
+    if tag_id:
+        selected_tag = get_object_or_404(Tags, pk=tag_id)   # NOw filter(...) gives a collection
+        posts = posts.filter(tags=selected_tag)             # get_object_or_404(...) gives one tag object
+                                                            # posts.filter(tags=selected_tag) works only with one tag object
+                                                            # Defeated the prob
+    return render(request, "posts/posts_view.html", context={ 
         "posts": posts,
         "tags": tags,
         "selected_tag": selected_tag,
